9c789f98124f73c5822b14c83f94a3e18a9f5f5e
Hubert Walczak <hubertwalczak8@gmail.com>
2023-05-04 13:13:42 +0200

    Added squads remaining

--- src/HUD/HUDPopup/index.tsx
+++ src/HUD/HUDPopup/index.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "./index.scss";
 import { Events } from 'apexlegendsgsi/types/events';
-import { ReactComponent as SquadEliminationIcon } from './squadEliminatedIcon.svg'
-export type EventTypes = keyof Events;
+import { ReactComponent as SquadEliminationIcon } from './squadEliminatedIcon.svg';
+import { ReactComponent as TeamsRemainingIcon } from './teamsRemainingIcon.svg'
+
+export type EventTypes = keyof Events | "squadsRemaining";
 export type ApexEvent = { content: string; id: string; type: EventTypes, icon?: React.ReactNode }
 
 
-const formatter: { [K in EventTypes]?: (content: string) => { text: string, icon: React.ReactNode } } = {
+const formatter: { [K in EventTypes]?: (content: string) => { text: React.ReactNode, icon: React.ReactNode } } = {
   squadEliminated: (content) => ({ text: `${content}`, icon: <div className="event-icon"><SquadEliminationIcon height={27} width={20} /></div> }),
   ringStartClosing: () => ({ text: `RING IS CLOSING`, icon: <div className="event-icon"><div className="circle"></div></div> }),
   ringFinishedClosing: () => ({ text: `RING IS CLOSED`, icon: <div className="event-icon"><div className="circle"></div></div> }),
   playerKilled: (content) => ({ text: content, icon: null }),
-  inventoryPickUp: (content) => ({ text: content, icon: null })
+  inventoryPickUp: (content) => ({ text: content, icon: null }),
+  squadsRemaining: (content) => ({ text: <><div className="">{content}</div> teams remaining</>, icon:<div className="event-icon"><TeamsRemainingIcon /></div> })
 };
 
 const Event = ({ content, type, customIcon }: { content: string, type: EventTypes, customIcon?: React.ReactNode}) => {
